0a6a9d64402bd55855f0fe2a515f5b27fcad30e3
Unknown <Aitor.vidarte@alumni.mondragon.edu>
2018-01-04 11:23:04 +0100

    FirstTest

---
+++ src/test/java/TestFuncionality/TestingTrainThread.java
@@ -0,0 +1,14 @@
+package TestFuncionality;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestingTrainThread {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
