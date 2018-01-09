ac69a317d110c95309eeab6ae3e39eaa92acc95a
Unknown <Aitor.vidarte@alumni.mondragon.edu>
2018-01-09 18:19:16 +0100

    Changin initialization of resourcesPool!!

--- src/main/java/Controller/ResourcesPool.java
+++ src/main/java/Controller/ResourcesPool.java
@@ -38,9 +38,8 @@ public class ResourcesPool {
 		iniciarCircuito(); //leyendo los datos de la base de datos y creando objetos.
 		asignarPaquetesAEstaciones(); //asignados los objetos paquete leidos de la base de datos a los objetos estacion.
 		createThreads();// creando los hilos tipo Tren pasandole el tren y el circuito.
-		
-		//launchThreads();
-		//asignarPaquetesAEstacionesYtrenes();
+//		launchThreads();// lanzando los hilos!
+
 		
 		
 		
@@ -75,44 +74,9 @@ public class ResourcesPool {
 		circuito.setRailes(rails);
 
 	}
-
-	private void asignarPaquetesAEstacionesYtrenes() {
-
-//		Station station = null;
-//		Train train = null;
-//		for (Package pack : packages) {
-//
-//			if (pack.getPackageState() == 0) {
-//
-//				station = pack.getOrigin();
-//				for (Station stat : stations) {
-//					if (stat.getStationID() == station.getStationID()) {
-//						stat.addNewPackageToSend(pack);
-//					}
-//				}
-//			} else if (pack.getPackageState() == 2) {
-//				station = pack.getDestination();
-//				for (Station stat : stations) {
-//					if (stat.getStationID() == station.getStationID()) {
-//						stat.addDeliveredPackageList(pack);
-//					}
-//				}
-//			} else {
-//				train = pack.getTakeTrain();
-//				for (TrainThread trains : trainThreads) {
-//					if (train.getTrainID() == trains.getTrain().getTrainID()) {
-//						trains.getTrain().addPackageList(pack);
-//					}
-//				}
-//				train.addPackageList(pack);
-//			}
-
-		}
-		
-		private void asignarPaquetesAEstaciones() {
+	private void asignarPaquetesAEstaciones() {
 
 			Station station = null;
-			Train train = null;
 			for (Package pack : packages) {
 
 				if (pack.getPackageState() == 0) {
@@ -134,13 +98,6 @@ public class ResourcesPool {
 				
 			}	
 		circuito.setEstaciones(stations);
-		for (Station stat : stations) {
-			System.out.println("Estacion: " + stat.getDescription() + " tiene " + stat.getSendPackageList().size()
-					+ " para enviar!");
-			System.out.println("Estacion: " + stat.getDescription() + " tiene " + stat.getDeliveredPackageList().size()
-					+ " entregados!");
-		}
-		
 		for (TrainThread trains : trainThreads) {
 			System.out.println("El tren "+trains.getTrain().getTrainID()+" tiene "+trains.getTrain().getPackageList().size()+" paquetes!");
 		}
@@ -171,6 +128,7 @@ public class ResourcesPool {
 		for (int i = 0; i < TRAINNUMBER; i++) {
 			System.out.println("El Tren:"+trains.get(i).getTrainID()+" esta en la estacion: " +trains.get(i).getStation().getDescription()+""
 					+ "y la estacion tiene "+ +trains.get(i).getStation().getSendPackageList().size()+" paquetes para recoger!");
+			
 			trainThreads.add(new TrainThread(trains.get(i), circuito));
 		}
 		
