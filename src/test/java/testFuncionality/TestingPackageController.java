10921a4258e98c8445a5038037add9f5676a5d3f
xanti <xanti.leonet@alumni.mondragon.edu>
2018-01-17 18:42:07 +0100

    testing prove

--- src/test/java/testFuncionality/TestingPackageController.java
+++ src/test/java/testFuncionality/TestingPackageController.java
@@ -12,23 +12,19 @@ import Modelo.Package;
 
 public class TestingPackageController {
 
-	Package paquete;
-	Station origin, destination;
-	PackageController packageController;
 	ResourcesPool resourcesPool;
-	
+	Package paquete1, paquete2;
 	@Before
 	public void setUp(){
-		origin = new Station(1, 43.2541730, -1.8474567, "Oiartzun", 2, 6, 1, 1, null, null);
-		destination = new Station(2, 43.1077539, -2.0800512, "Tolosa", 3, 1, 1, 1, null, null);
-		paquete = new Package(origin, destination, "Paquete 1");
 		resourcesPool = new ResourcesPool();
-		packageController = new PackageController(resourcesPool);
+		resourcesPool.iniciarCircuito();
+		paquete1 = new Package(resourcesPool.getStations().get(0), resourcesPool.getStations().get(2), "Paquete 1");
+		paquete2 = new Package(resourcesPool.getStations().get(2), resourcesPool.getStations().get(0), "Paquete 2");
 	}
 	
 	@Test
 	public void testCalcularDireccionPaquete1() {
-		assertEquals(0, packageController.calcularDireccionPaquete(paquete));
+		assertEquals(0, resourcesPool.calcularDireccionPaquete(paquete1));
 	}
 	
 	@Test
