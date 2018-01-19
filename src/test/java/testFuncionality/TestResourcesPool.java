507ceef151120df9587b6b236d94a3354a11de0e
xanti <xanti.leonet@alumni.mondragon.edu>
2018-01-19 19:34:43 +0100

    Testing threads

--- src/test/java/testFuncionality/TestResourcesPool.java
+++ src/test/java/testFuncionality/TestResourcesPool.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -11,40 +12,29 @@ import org.junit.Test;
 import Controller.ResourcesPool;
 import Modelo.Package;
 import Modelo.Station;
+import Modelo.Train;
 
 public class TestResourcesPool {
-	Station oiartzun, tolosa, azpeitia, zumaia, zarautz, donostia;
+	ArrayList<Station> stations;
+	ArrayList<Package> packages;
+	ArrayList<Train> trains;
+
+	
 	Package paquete1, paquete2;
 	ResourcesPool resourcesPool;
 	Method calcularDireccionPaquete, distanciaEntreEstaciones;
+
 	@Before
 	public void setUp() {
-		oiartzun = new Station();
 		resourcesPool = new ResourcesPool();
+		stations = new ArrayList<Station>();
+		trains = new ArrayList<Train>();
+		packages = new ArrayList<Package>();
+		initStations();
+		initPackages();
 
-		oiartzun = createStation(1, "Oiartzun");
-		tolosa = createStation(2, "Tolosa");
-		azpeitia = createStation(3, "Azpeitia");
-		zumaia = createStation(4, "Zumaia");
-		zarautz = createStation(5, "Zarautz");
-		donostia = createStation(6, "Donostia");
-
-		oiartzun.setNextStation(tolosa);
-		oiartzun.setPreviousStation(donostia);
-		tolosa.setNextStation(azpeitia);
-		tolosa.setPreviousStation(oiartzun);
-		azpeitia.setNextStation(zumaia);
-		azpeitia.setPreviousStation(tolosa);
-		zumaia.setNextStation(zarautz);
-		zumaia.setPreviousStation(azpeitia);
-		zarautz.setNextStation(donostia);
-		zarautz.setPreviousStation(zumaia);
-		donostia.setNextStation(oiartzun);
-		donostia.setPreviousStation(zarautz);
-
-		paquete1 = new Package(oiartzun, azpeitia, "Paquete 1");
-		paquete2 = new Package(azpeitia, oiartzun, "Paquete 2");
 		
+
 		try {
 			calcularDireccionPaquete = resourcesPool.getClass().getDeclaredMethod("calcularDireccionPaquete",
 					Package.class);
@@ -62,7 +52,7 @@ public class TestResourcesPool {
 	@Test
 	public void testCalcularDireccionPaquete1() {
 		try {
-			assertEquals(0, calcularDireccionPaquete.invoke(resourcesPool, paquete1));
+			assertEquals(0, calcularDireccionPaquete.invoke(resourcesPool, packages.get(0)));
 		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -72,7 +62,7 @@ public class TestResourcesPool {
 	@Test
 	public void testCalcularDireccionPaquete2() {
 		try {
-			assertEquals(1, calcularDireccionPaquete.invoke(resourcesPool, paquete2));
+			assertEquals(1, calcularDireccionPaquete.invoke(resourcesPool, packages.get(1)));
 		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -81,27 +71,53 @@ public class TestResourcesPool {
 
 	@Test
 	public void testDistanciaEntreEstacionesDir0() {
-			try {
-				assertEquals(2,distanciaEntreEstaciones.invoke(resourcesPool, oiartzun, azpeitia,0));
-			} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		try {
+			assertEquals(2, distanciaEntreEstaciones.invoke(resourcesPool, stations.get(0), stations.get(2), 0));
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Test
 	public void testDistanciaEntreEstacionesDir1() {
-			try {
-				assertEquals(4,distanciaEntreEstaciones.invoke(resourcesPool, oiartzun, azpeitia,1));
-			} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		try {
+			assertEquals(4, distanciaEntreEstaciones.invoke(resourcesPool, stations.get(0), stations.get(2), 1));
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+
 	public Station createStation(int ID, String description) {
 		Station newStation = new Station();
 		newStation.setStationID(ID);
 		newStation.setDescription(description);
 		return newStation;
+	}
+	public void initStations() {
+		stations.add(createStation(1, "Oiartzun"));
+		stations.add(createStation(2, "Tolosa"));
+		stations.add(createStation(3, "Azpeitia"));
+		stations.add(createStation(4, "Zumaia"));
+		stations.add(createStation(5, "Zarautz"));
+		stations.add(createStation(6, "Donostia"));
+
+		stations.get(0).setNextStation(stations.get(1));
+		stations.get(0).setPreviousStation(stations.get(5));
+		stations.get(1).setNextStation(stations.get(2));
+		stations.get(1).setPreviousStation(stations.get(0));
+		stations.get(2).setNextStation(stations.get(3));
+		stations.get(2).setPreviousStation(stations.get(1));
+		stations.get(3).setNextStation(stations.get(4));
+		stations.get(3).setPreviousStation(stations.get(2));
+		stations.get(4).setNextStation(stations.get(5));
+		stations.get(4).setPreviousStation(stations.get(3));
+		stations.get(5).setNextStation(stations.get(0));
+		stations.get(5).setPreviousStation(stations.get(4));
+	}
+	public void initPackages() {
+		packages.add(new Package(stations.get(0), stations.get(2), "Paquete 1"));
+		packages.add(new Package(stations.get(2), stations.get(0), "Paquete 2"));
 	}
 }
